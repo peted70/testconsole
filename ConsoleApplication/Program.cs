6faa953303b2189f1d50f61038b9cbae56880c28
peted70 <peted70@hotmail.com>
2015-02-22 14:45:16 +0000

    change 2 from thinkpad

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication
         {
             // Change made from DEll
             // local change from ThinkPad
+            // local change 2 from ThinkPad
         }
     }
 }
